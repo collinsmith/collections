60848e0844a5a8e9f182de82da166eab3ab054dd
Collin Smith <collinsmith70@gmail.com>
2015-10-27 21:28:05 -0700

    Added Trie along with TernaryTrie impl

---
+++ src/main/java/com/gmail/collinsmith70/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Aggregation of various data structures that I have implementations for.
+ * 
+ * <a href="https://github.com/collinsmith/collections">Github Repository</a>
+ * 
+ * @author Collin Smith <i>collinsmith70@gmail.com</i>
+ */
+package com.gmail.collinsmith70.util;
