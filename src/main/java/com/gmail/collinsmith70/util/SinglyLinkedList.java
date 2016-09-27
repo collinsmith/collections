0d8de12db7b61f275d63914826f3f16157f706e2
Collin Smith <collinsmith70@gmail.com>
2016-09-27 13:04:10 -0700

    Added SinglyLinkedList

---
+++ src/main/java/com/gmail/collinsmith70/util/SinglyLinkedList.java
@@ -0,0 +1,20 @@
+package com.gmail.collinsmith70.util;
+
+import java.util.AbstractList;
+
+public class SinglyLinkedList<E> extends AbstractList<E> {
+
+  private SinglyLinkedList() {
+  }
+
+  @Override
+  public int size() {
+    return 0;
+  }
+
+  @Override
+  public E get(int index) {
+    return null;
+  }
+
+}
