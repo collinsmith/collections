2da45ea1cae865e81cb7ba7fd232b83afca59273
Collin Smith <collinsmith70@gmail.com>
2016-10-31 18:06:52 -0700

    Added test for List#remove(Object)

--- src/test/java/com/gmail/collinsmith70/util/ListTest.java
+++ src/test/java/com/gmail/collinsmith70/util/ListTest.java
@@ -26,4 +26,14 @@ public class ListTest<T extends List> {
     assertTrue(instance.contains(obj));
   }
 
+  @Test
+  public void test_remove() throws IllegalAccessException, InstantiationException {
+    Object obj = new Object();
+    T instance = clazz.newInstance();
+    instance.add(obj);
+    assertTrue(instance.contains(obj));
+    Object removed = instance.remove(obj);
+    assertTrue(obj.equals(removed));
+  }
+
 }
