8120faf97e9d7b34365139e5c87747d4d7c858ef
Collin Smith <collinsmith70@gmail.com>
2016-12-06 16:52:03 -0800

    Added package-info.java

---
+++ src/com/gmail/collinsmith70/collections/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains a collection of data structures which I have implementations for in the
+ * Java Programming Language.
+ *
+ * @author Collin Smith
+ */
+package com.gmail.collinsmith70.collections;
