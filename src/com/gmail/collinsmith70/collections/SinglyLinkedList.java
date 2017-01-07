82a5703c48a890540f4a5bfc9f6d26dc588cbfdd
Collin Smith <collinsmith70@gmail.com>
2017-01-06 16:06:30 -0800

    Implemented simpler implementations

--- src/com/gmail/collinsmith70/collections/SinglyLinkedList.java
+++ src/com/gmail/collinsmith70/collections/SinglyLinkedList.java
@@ -1,40 +1,89 @@
 package com.gmail.collinsmith70.collections;
 
-import java.util.ListIterator;
+public class SinglyLinkedList<E> {
 
-public class SinglyLinkedList<E> extends AbstractSinglyLinkedList<E, SinglyLinkedList.Node<E>> {
+  Node<E> first;
+  Node<E> last;
+  int size;
 
   public SinglyLinkedList() {
   }
 
-  @Override
-  public Node<E> get() {
-    return new Node<>();
+  public int size() {
+    return size;
   }
 
-  protected static class Node<E> extends AbstractSinglyLinkedList.Node<E, Node<E>> {
+  public void addFirst(E element) {
+    if (first == null) {
+      first = last = new Node<>(element, null);
+    }
+
+    Node<E> n = new Node<>(element, first);
+    first = n;
+  }
+
+  public void addLast(E element) {
+    if (first == null) {
+      first = last = new Node<>(element, null);
+      return;
+    }
+
+    Node<E> n = new Node<>(element, null);
+    last.next = n;
+    last = n;
+  }
+
+  public E removeFirst() {
+    if (first == null) {
+      return null;
+    }
+
+    E element = first.element;
+    first = first.next;
+    return element;
+  }
+
+  public E removeLast() {
+    if (last == null) {
+      return null;
+    }
+
+    E element = last.element;
+    last = getPrevious(last);
+    return element;
+  }
+
+  private Node<E> getPrevious(Node<E> node) {
+    Node<E> prev = null;
+    for (Node<E> n = first; n != null; prev = n, n = n.next);
+    return prev;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(getClass().getSimpleName());
+    sb.append(":{");
+    for (Node<E> n = first; n != null; n = n.next) {
+      sb.append(n.element);
+    }
+
+    sb.append("}");
+    return sb.toString();
+  }
+
+  static class Node<E> {
+    E element;
+    Node<E> next;
+
     Node() {
       this(null, null);
     }
 
     Node(E element, Node<E> next) {
-      super(element, next);
+      this.element = element;
+      this.next = next;
     }
-  }
-
-  @Override
-  public ListIterator<E> listIterator(int index) {
-    checkElementIndex(index);
-    return new SinglyLinkedListIterator(index);
-  }
-
-  protected class SinglyLinkedListIterator
-      extends AbstractSinglyLinkedList<E, Node<E>>.SinglyLinkedListIterator {
-
-    SinglyLinkedListIterator(int index) {
-      super(index);
-    }
-
   }
 
 }
