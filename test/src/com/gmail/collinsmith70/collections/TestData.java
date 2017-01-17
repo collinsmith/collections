0074db1046527c933dbc6a6c9523bb2c620efbaa
Collin Smith <collinsmith70@gmail.com>
2017-01-17 15:39:00 -0800

    Create java.util.List field to store List of wrapped prime objects (was creating many of these on the fly before)

--- test/src/com/gmail/collinsmith70/collections/TestData.java
+++ test/src/com/gmail/collinsmith70/collections/TestData.java
@@ -1,5 +1,7 @@
 package com.gmail.collinsmith70.collections;
 
+import java.util.Arrays;
+
 public class TestData {
 
   static final int[] PRIMES = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
@@ -10,6 +12,11 @@ public class TestData {
     for (int i = 0; i < PRIMES.length; i++) {
       WRAPPED_PRIMES[i] = PRIMES[i];
     }
+  }
+
+  static final java.util.List<Integer> LISTED_PRIMES;
+  static {
+    LISTED_PRIMES = Arrays.asList(WRAPPED_PRIMES);
   }
 
   static final int[] SORTING_PRIMES = {29, 17, 11, 3, 7, 19, 5, 2, 23, 13};
